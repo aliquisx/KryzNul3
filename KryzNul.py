c160844bfde845d9d39c2f2f6821e3f475ef3a3b
Erikas Šulskis <erikassulskis12@gmail.com>
2024-09-18 09:08:50 +0300

    Update KryzNul.py

--- KryzNul.py
+++ KryzNul.py
@@ -1,12 +1,13 @@
+#nurodo žaidmo lentą
 def rodyti_lenta(lenta):
     for i in range(0, 9, 3):
         print(' | '.join(lenta[i:i+3]))
         if i < 6: print("---------")
-
+#tikrina laimėjimų kombinacijas
 def tikrinti_laimejima(lenta, zaidejas):
     pergales = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
     return any(all(lenta[i] == zaidejas for i in kombinacija) for kombinacija in pergales)
-
+#kryžiukų-nuliukų veiksmų eiga
 def zaisti():
     lenta, zaidejas = [' '] * 9, 'X'
     while ' ' in lenta:
